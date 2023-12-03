cdea1f40e39f408c9d172dc2b269e05615309adc
alexi <alexi@LAPTOP-B08T0I2S>
2023-12-02 23:26:40 -0800

    Se incluyo CRUD para todos los modelos y se agrego imagen.

---
+++ Eportafolio/Controllers/ProyectosController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Eportafolio.Models;
+using System.IO;
+
+namespace Eportafolio.Content
+{
+    public class ProyectosController : Controller
+    {
+        private avillasenorEntities db = new avillasenorEntities();
+
+        // GET: Proyectos
+        public ActionResult Index()
+        {
+            return View(db.Proyectos.ToList());
+        }
+
+        // GET: Proyectos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyectos proyectos = db.Proyectos.Find(id);
+            if (proyectos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyectos);
+        }
+
+        // GET: Proyectos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Proyectos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Descripcion,Imagen,Empresa,Fecha")] Proyectos proyectos, HttpPostedFileBase ImagenFile)
+        {
+            if (ModelState.IsValid)
+            {
+                //Verifica si se proporciono un archivo de imagen
+                if(ImagenFile !=null && ImagenFile.ContentLength >0)
+                {
+                    //Obtener el nombre del archivo y el path donde se guarda en la carpeta
+                    var filename = Path.GetFileName(ImagenFile.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Content/Img/"), filename);
+
+                    //Guarda el archivo en el path especificado
+                    ImagenFile.SaveAs(path);
+
+                    //Asigna la URL de la Imagen al campo "Imagen" del modelo
+                    proyectos.Imagen = filename;
+                }
+
+                db.Proyectos.Add(proyectos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(proyectos);
+        }
+
+        // GET: Proyectos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyectos proyectos = db.Proyectos.Find(id);
+            if (proyectos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyectos);
+        }
+
+        // POST: Proyectos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Descripcion,Imagen,Empresa,Fecha")] Proyectos proyectos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proyectos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(proyectos);
+        }
+
+        // GET: Proyectos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyectos proyectos = db.Proyectos.Find(id);
+            if (proyectos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyectos);
+        }
+
+        // POST: Proyectos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Proyectos proyectos = db.Proyectos.Find(id);
+            db.Proyectos.Remove(proyectos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
